824979f8b18ced9e49d9d8f8449c99d92001413d
kimyongjun413 <kimyongjun413@gmail.com>
2019-07-29 21:27:54 +0900

    [Create] PlayerServiceTests

---
+++ src/test/java/com/sjproject/cpmadminservice/application/PlayerServiceTests.java
@@ -0,0 +1,49 @@
+package com.sjproject.cpmadminservice.application;
+
+import com.sjproject.cpmadminservice.domain.*;
+import org.assertj.core.api.AssertDelegateTarget;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.validation.constraints.AssertTrue;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PlayerServiceTests {
+
+    private PlayerService playerService;
+
+    @Before
+    public void setUp() {
+        HitterRepository hitterRepository = new HitterRepositoryImpl();
+        PitcherRepository pitcherRepository = new PitcherRepositoryImpl();
+        playerService = new PlayerService(hitterRepository, pitcherRepository);
+
+    }
+
+    @Test
+    public void getHitter() {
+        Hitter hitter = playerService.getHitter("강동우");
+        assertThat(hitter.getName(), is("강동우"));
+    }
+
+    @Test
+    public void getHitters() {
+        List<Hitter> hitters = playerService.getHitters();
+        assertThat(hitters.get(1).getName(), is("강동우"));
+    }
+
+    @Test
+    public void getPitcher() {
+        Pitcher pitcher = playerService.getPitcher("가득염");
+        assertThat(pitcher.getName(), is("가득염"));
+    }
+
+    @Test
+    public void getPitchers() {
+        List<Pitcher> pitchers = playerService.getPitchers();
+        assertThat(pitchers.get(1).getName(), is("가득염"));
+    }
+}
